4aadbd2b075e2c8e33fee7448069197d74fed24e
Adeline A. Ackun <adelineackun@gmail.com>
2025-06-30 11:51:40 +0000

    Payment error fix

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ import connectCloudinary from "./config/cloudinary.js"
 import userRouter from "./routes/userRoute.js"
 import doctorRouter from "./routes/doctorRoute.js"
 import adminRouter from "./routes/adminRoute.js"
-import paymentRouter from './routes/paymentRoute.js';
 
 
 // app config
@@ -23,7 +22,6 @@ app.use(cors())
 app.use("/api/user", userRouter)
 app.use("/api/admin", adminRouter)
 app.use("/api/doctor", doctorRouter)
-app.use('/api/payment', paymentRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working")
